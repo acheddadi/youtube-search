276e3d1b341ac7229b722bdaa82cc743bc052117
Ali Cheddadi <alicheddadi@gmail.com>
2022-11-04 10:15:23 -0400

    Fixed Iteration Issue
    Altered looping logic after a YT update. Also include a bash
    script for reference.

--- yts.py
+++ yts.py
@@ -15,16 +15,23 @@ response = urllib.request.urlopen(url).read().decode();
 videoIds = re.findall(r"watch\?v=(\S{11})", response);
 
 i = 0;
+j = 0;
+lastId = "";
 for id in videoIds:
     if (i >= count): break;
+    if (lastId == id):
+        j += 1;
+        continue;
+    lastId = id;
     entry = {"format": "json", "url": WATCH_URL + "%s" % id}
     query = urllib.parse.urlencode(entry)
     url = "https://www.youtube.com/oembed?" + query
 
     response = urllib.request.urlopen(url).read();
     data = json.loads(response.decode());
-    print(str(i) + ". " + data['title'] + "\n" + WATCH_URL + id);
+    print(str(j) + ". " + data['title'] + "\n" + WATCH_URL + id);
     i += 1;
+    j += 1;
 
 choice = int(input("Enter a video to play: "));
 choice = max(0, min(choice, len(videoIds) - 1));
